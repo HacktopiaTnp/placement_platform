4afe82597458d4d71eae37c11786fffd928bd091
devraushan <devraushansingh@gmail.com>
2026-02-01 01:57:57 +0530

    feat: add interview component

---
+++ mockinterview/app/dashboard/interview/[interviewId]/start/page.jsx
@@ -0,0 +1,72 @@
+"use client";
+import React, { useState } from "react";
+import { useEffect } from "react";
+import QuestionSection from "./_components/QuestionSection";
+import RecordAnswerSection from "./_components/RecordAnswerSection";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+const StartInterview = ({ params }) => {
+  const [interviewData, setInterviewData] = useState();
+  const [mockInterviewQuestion, setMockInterviewQuestion] = useState();
+  const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
+  useEffect(() => {
+    GetInterviewDetails();
+  }, []);
+
+  const GetInterviewDetails = async () => {
+    const response = await fetch(`/api/interview?mockId=${params.interviewId}`);
+    const result = await response.json();
+
+    if (result.success && result.data.length > 0) {
+      const jsonMockResp = JSON.parse(result.data[0].jsonMockResp);
+      console.log(jsonMockResp);
+      setMockInterviewQuestion(jsonMockResp);
+      setInterviewData(result.data[0]);
+    }
+  };
+
+  return (
+    <div>
+      <div className="grid grid-cols-1 md:grid-cols-2 my-10">
+        {/* Questin Section */}
+        <QuestionSection
+          mockInterviewQuestion={mockInterviewQuestion}
+          activeQuestionIndex={activeQuestionIndex}
+        />
+
+        {/* Video/audio Recording */}
+        <RecordAnswerSection
+          mockInterviewQuestion={mockInterviewQuestion}
+          activeQuestionIndex={activeQuestionIndex}
+          interviewData={interviewData}
+        />
+      </div>
+      <div className="flex gap-3 my-5 md:my-0 md:justify-end md:gap-6">
+        {activeQuestionIndex > 0 && (
+          <Button
+            onClick={() => setActiveQuestionIndex(activeQuestionIndex - 1)}
+          >
+            Previous Question
+          </Button>
+        )}
+        {activeQuestionIndex != mockInterviewQuestion?.length - 1 && (
+          <Button
+            onClick={() => setActiveQuestionIndex(activeQuestionIndex + 1)}
+          >
+            Next Question
+          </Button>
+        )}
+        {activeQuestionIndex == mockInterviewQuestion?.length - 1 && (
+          <Link
+            href={"/dashboard/interview/" + interviewData?.mockId + "/feedback"}
+          >
+            <Button>End Interview</Button>
+          </Link>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default StartInterview;
